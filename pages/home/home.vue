c28ddfd96299fcb6bb6dc530ccbb64c249f7caca
M M <mm@MdeMacBook-Pro.local>
2021-12-23 14:42:36 +0800

    首页功能实现-导航楼层栏

--- pages/home/home.vue
+++ pages/home/home.vue
@@ -10,28 +10,43 @@
     </swiper>
     <!-- 分类导航区域 -->
     <view class="nav-list">
-        <view class="nav-item" v-for="(item,j) in navList" :key="j" >
+        <view class="nav-item" v-for="(item,j) in navList" :key="j" @click="navClickHandler(item)">
           <navigator url="">
             <img :src="item.image_src" alt="" class="nav-img">
           </navigator>
         </view>
     </view>
+	<!-- 导航楼层区域 -->
+	  <view class="floor-list">
+		  <!-- 每个楼层内容 -->
+	    <view class="floor-item" v-for="(item,i) in floorList" :key="i">
+			<!-- 楼层标题 -->
+	      <img :src="item.floor_title.image_src" alt="" class="floor-title">
+		  <!-- 楼层图片区域 -->
+		  <view class="floor-img-box">
+			  <!-- 左侧大图盒子 -->
+			  <view class="left-img-box">
+				  <img :src="item.product_list[0].image_src" :style="{width: item.product_list[0].image_width + 'rpx'}" alt="" mode="widthFix">
+			  </view>
+			  <!-- 右侧4个小图片盒子 -->
+			  <view class="right-img-box">
+				  <view class="right-img-item" v-for="(item2,i2) in item.product_list" :key="i2" v-if="i2 !== 0">
+					  <img :src="item2.image_src" :style="{width: item2.image_width + 'rpx'}" mode="widthFix" alt="">
+				  </view>
+			  </view>
+		  </view>
+	    </view>
+	  </view>
   </view>
-  <!-- 导航楼层区域 -->
-    <view class="floor-list">
-      <view class="floor-item">
-        <img src="" alt="">
-      </view>
-    </view>
 </template>
 
 <script>
   export default {
     data() {
       return {
-        swiperList: [],
-        navList: [],
-        floorList: []
+        swiperList: [], //轮播图数据
+        navList: [], //导航栏数据
+        floorList: [], //楼层数据
       };
     },
     onLoad() {
@@ -61,7 +76,16 @@
         console.log(res);
         if(res.meta.status !== 200) return uni.$showMsg();
         this.floorList = res.message;
-      }
+		// console.log(this.floorList);
+      },
+	  //点击跳转不同的分类页面
+	  navClickHandler(item) {
+		  if(item.name === "分类") {
+			  uni.switchTab({
+			  	url:"/pages/cate/cate"
+			  })
+		  }
+	  }
     }
   }
 </script>
@@ -84,5 +108,21 @@
      width: 128rpx;
      height: 140rpx;
    }
+ }
+ .floor-title {
+	height: 60rpx;
+	width: 100%;
+	display: flex;
+ }
+ 
+ .right-img-box {
+	 display: flex;
+	 flex-wrap: wrap;
+	 justify-content: space-around;
+ }
+ 
+ .floor-img-box {
+	 display: flex;
+	 padding-left: 10rpx;
  }
 </style>
