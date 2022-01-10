c7e6d324f6e68ba8472c287a02be7a7604d21bc3
saucyjackme <saucyjackyme@163.com>
2022-01-10 11:24:03 +0800

    上拉加载更多+通过节流阀发起额外的请求

--- subpkg/goods_list/goods_list.vue
+++ subpkg/goods_list/goods_list.vue
@@ -21,6 +21,7 @@
         },
         goodsList: [],
         total: 0, //分页
+        isLoading: false // 当前是否正在请求数据
       };
     },
     onLoad(options) {
@@ -33,16 +34,30 @@
     methods: {
       //获得商品列表数据
       async getGoodsList() {
+        //打开节流阀
+        this.isLoading = true;
         const result = uni.$http.get('/api/public/v1/goods/search', this.queryObj);
         console.log(result);
         //解构data属性重命名为res
         const {
           data: res
         } = await uni.$http.get('/api/public/v1/goods/search', this.queryObj);
+        //关闭节流阀
+        this.isLoading = false;
         if (res.meta.status !== 200) return uni.$showMsg()
-        this.goodsList = res.message.goods;
+        // 为数据赋值：通过展开运算符的形式，进行新旧数据的拼接
+        this.goodsList = [...this.goodsList, ...res.message.goods];
         this.total = res.message.total;
+   
       }
+    },
+    onReachBottom() {
+      //判断节流阀是否开启来确定上拉加载更多事件,判断是否正在请求其它数据，如果是，则不发起额外的请求，直接返回；如果不是【即没有正在请求数据】则开始自增
+      if (this.isloading) return
+      //触发时，让页码自增加1
+      this.queryObj.pagenum += 1;
+      // 重新获取列表数据
+      this.getGoodsList()
     }
   }
 </script>
