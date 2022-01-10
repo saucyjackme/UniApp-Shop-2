45c38d3e097ae2c98e416be668001bef56b4bb63
saucyjackme <saucyjackyme@163.com>
2022-01-10 15:30:35 +0800

    下拉刷新事件（重置第一页请求数据并且改造getGoodsList函数）

--- subpkg/goods_list/goods_list.vue
+++ subpkg/goods_list/goods_list.vue
@@ -33,17 +33,18 @@
     },
     methods: {
       //获得商品列表数据
-      async getGoodsList() {
+      async getGoodsList(cb) {
         //打开节流阀
         this.isLoading = true;
-        const result = uni.$http.get('/api/public/v1/goods/search', this.queryObj);
-        console.log(result);
+        // const result = uni.$http.get('/api/public/v1/goods/search', this.queryObj);
+        // console.log(result);
         //解构data属性重命名为res
         const {
           data: res
         } = await uni.$http.get('/api/public/v1/goods/search', this.queryObj);
         //关闭节流阀
         this.isLoading = false;
+        cb && cb() //这里的callback相当于()=>{uni.stopPullDownRefresh();
         if (res.meta.status !== 200) return uni.$showMsg()
         // 为数据赋值：通过展开运算符的形式，进行新旧数据的拼接
         this.goodsList = [...this.goodsList, ...res.message.goods];
@@ -51,13 +52,26 @@
    
       }
     },
-    onReachBottom() {
+    onReachBottom() {//上拉加载处理
+      //判断数据是否加载完成
+      if (this.queryObj.pagenum * this.queryObj.pagesize >= this.total) return uni.$showMsg('数据加载完毕！')
       //判断节流阀是否开启来确定上拉加载更多事件,判断是否正在请求其它数据，如果是，则不发起额外的请求，直接返回；如果不是【即没有正在请求数据】则开始自增
-      if (this.isloading) return
+      if (this.isLoading) return
       //触发时，让页码自增加1
       this.queryObj.pagenum += 1;
       // 重新获取列表数据
       this.getGoodsList()
+    },
+    onPullDownRefresh() {// 监听下拉刷新事件
+      //重置关键数据，恢复原始第一页数据请求
+      this.queryObj.pagenum = 1;
+      this.total = 0
+      this.isLoading = false;
+      thi.goodsList = [];
+      //重新发起请求
+      this.getGoodsList(()=>{
+        uni.stopPullDownRefresh();//停止下拉刷新
+      })
     }
   }
 </script>
